02e315ebe90b67b3c8bef7d8056db8861d9811bd
ain <ain@dummy.com>
2016-08-01 01:18:33 +0900

    add .zshrc

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dotfiles
 My dotfiles
+
+# Clone
+$ git clone https://github.com/ain0204/dotfiles.git ~/dotfiles --recursive
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
+
+# setup oh-my-zsh
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+~/dotfiles/oh-my-zsh/tools/install.sh
